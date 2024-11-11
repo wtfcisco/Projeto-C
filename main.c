c5528b09c900330403fe8a855d67ccf00f2c4816
wtfcisco <franciscopaulino100@outlook.com>
2024-11-10 21:37:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
-#include "cadastro.c"
+#include "functions.c"
+//Por enquanto usando a main apenas para testar funções
 int main() {
-    
-    // O main até agora eu usei apenas para testar se todos os dados estão sendo armazenados corretamente no vetor usuarios e sim, funcionou.
-
     cadastrar_usuario();
-
-    for (int i = 0; i < contador_usuarios; i++) {
-        printf("\n--- Dados do Usuário %d ---\n", i + 1);
-        printf("Nome: %s\n", usuarios[i].nome);
-        printf("CPF: %s\n", usuarios[i].cpf);
-        printf("Data de Nascimento: %s\n", usuarios[i].dataNascimento);
-        printf("E-mail: %s\n", usuarios[i].email);
-        printf("Telefone: %s\n", usuarios[i].telefone);
-        printf("Senha: %s\n", usuarios[i].senha);
-        printf("Conta Bancária: %d\n", usuarios[i].conta_bancaria);
-    }
+    realizar_login();
     return 0;
 }
