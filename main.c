cd4e5134b361408484a7664752bbdbad33689f9d
wtfcisco <franciscopaulino100@outlook.com>
2024-11-12 21:12:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,46 +2,45 @@
 #include "functions.c"
 
 int main() {
-    
-    int usuario = 0, opcao = 0;
+    int usuario, opcao = 0;
 
     while (opcao != 4) {
         printf("\n============================\n");
         printf("1 - Login\n");
         printf("2 - Cadastrar-se\n");
         printf("3 - Esqueci minha senha\n");
-        printf("4 - Sair");
-        printf("\n============================\n");
+        printf("4 - Sair\n");
+        printf("============================\n");
         scanf("%d", &opcao);
 
         switch (opcao) {
             case 1:
                 usuario = realizar_login();
+                if (usuario == -1) {
+                    printf("Falha no login, tente novamente.\n");
+                } else {
+                    menu_bancario(usuario);  // Exibe menu de operações bancárias
+                    usuario = -1;  // Reseta o usuário após sair do menu bancário
+                }
                 break;
+
             case 2:
                 cadastrar_usuario();
-                continue;
+                break;
+
             case 3:
                 resetar_senha();
-                continue;
-            case 4:
                 break;
+
+            case 4:
+                printf("Saindo...\n");
+                break;
+
             default:
                 printf("Digite uma opção válida\n");
-                continue;
-        }
-        if (usuario != -1 && opcao != 4) {
-            printf("\n============================\n");
-            printf("1 - Deposito\n");
-            printf("2 - Saque\n");
-            printf("3 - Transferencia\n");
-            printf("4 - Exibir Dados\n");
-            printf("5 - Sair");
-            printf("\n============================\n");
-            scanf("%d", &opcao);
-            if (opcao == 4) {
-                exibir_dados(usuario);
-            }
+                break;
         }
     }
+
+    return 0;
 }
