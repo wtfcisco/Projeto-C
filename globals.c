e8897767b8468d15ce506844332bdd476568ca63
wtfcisco <franciscopaulino100@outlook.com>
2024-11-15 22:45:40 -0300

    Update globals.c

--- globals.c
+++ globals.c
@@ -1,6 +1,14 @@
-#define MAX_USUARIOS 5
+//Define uma constante limitando o número de usuários cadastrados. Usamos bastante no código para poder iterar todos os usuários cadastrados no vetor com loop for
+#define MAX_USUARIOS 100
+#define MAX_TRANSACAO 100
+ 
+typedef struct {
+    char descricao[100]; // Ex.: "Depósito", "Saque", "Transferência"
+    float valor;
+    char dataHora[20];
+    char nome_destinatario[50];
+} Extrato;
 
- //Define uma constante limitando o número de usuários cadastrados. Usamos bastante no código para poder iterar todos os usuários cadastrados no vetor com loop for
 
 //Define uma typedef struct. Uma estrutura de dados que pode conter mais de um tipo de dados, e podemos nomear essa estrutura como quisermos, chamei-a de Dados.
 typedef struct {
@@ -12,40 +20,49 @@ typedef struct {
     char senha[20];
     int conta_bancaria;
     float saldo;
+    int contador_transacoes;
+    Extrato transacao[MAX_TRANSACAO];    
 } Dados; 
 
 // Fiz um cadastro para nós 3 para que pudessemos testar a função de cadastro e de login.
 Dados usuarios[MAX_USUARIOS] = {
     {//indice 0
-        "Francisco Paulino",
-        "00000000000",
-        "00/00/0000",
-        "francisco@paulino.com",
-        "00000000000",
-        "000000",
-        12345678,
-        10000
+        .nome = "Francisco Paulino",
+        .cpf = "00000000000",
+        .dataNascimento = "00/00/0000",
+        .email = "francisco@paulino.com",
+        .telefone= "00000000000",
+        .senha = "000000",
+        .conta_bancaria = 12345678,
+        .saldo = 10000,
+        .transacao = {{}},
+        .contador_transacoes = 0       
     },
     {//indice 1
-        "Matheus Barbosa",
-        "11111111111",
-        "00/00/0000",
-        "matheus@barbosa.com",
-        "00000000000",
-        "111111",
-        87654321,
-        10000
+        .nome = "Matheus Barbosa",
+        .cpf = "11111111111",
+        .dataNascimento = "00/00/0000",
+        .email = "matheus@barbosa.com",
+        .telefone= "00000000000",
+        .senha = "111111",
+        .conta_bancaria = 87654321,
+        .saldo = 10000,
+        .transacao = {{}},
+        .contador_transacoes = 0
     },
     {// indice 2
-        "Luan Kennedy",
-        "22222222222",
-        "00/00/0000",
-        "luan@kennedy.com",
-        "00000000000",
-        "222222",
-        01234567,
-        10000
+        .nome = "Luan Kennedy",
+        .cpf = "22222222222",
+        .dataNascimento = "00/00/0000",
+        .email = "luan@kennedy.com",
+        .telefone= "00000000000",
+        .senha = "222222",
+        .conta_bancaria = 01234567,
+        .saldo = 10000,
+        .transacao = {{}},
+        .contador_transacoes = 0
     }
 };
 
 int contador_usuarios = 3; //Contador para garantir que novos usuários sejam cadastrados sempre no próximo indice do vetor. O valor é incrementado a cada cadastro de usuário
+char dataHora[20] = "";
