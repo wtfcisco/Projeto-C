1bc884d8d2a3f5334d533f1b7e6eed4783e42f5c
wtfcisco <franciscopaulino100@outlook.com>
2024-11-25 22:23:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include "functions.c"
+#include "globals.h" // Estruturas e Variáveis globais
+#include "functions.h" // Funções do projeto
 
 int main() {
     int usuario, opcao = 0;
 
-    while (opcao != 4) {
+    while (opcao != 4) { //loop irá repetir enquanto a opção não for 4, sair
         printf("\n============================\n");
         printf("1 - Login\n");
         printf("2 - Cadastrar-se\n");
@@ -15,11 +16,11 @@ int main() {
 
         switch (opcao) {
             case 1:
-                usuario = realizar_login();
-                if (usuario == -1) {
+                usuario = realizar_login(); // a função realizar_login retorna o indice do usuario logado no vetor, caso usuario ou senha incorreto retorna -1
+                if (usuario == -1) { 
                     printf("Falha no login, tente novamente.\n");
                 } else {
-                    menu_bancario(usuario);  // Exibe menu de operações bancárias
+                    menu_bancario(usuario);  // Recebe como parametro o usuário logado e acessa o menu desse usuario
                     usuario = -1;  // Reseta o usuário após sair do menu bancário                    
                 }
                 break;
