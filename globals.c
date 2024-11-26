e5115a149d6f07297f8742c41851ef0fcfedf2ad
wtfcisco <franciscopaulino100@outlook.com>
2024-11-25 22:22:48 -0300

    Update globals.c

--- globals.c
+++ globals.c
@@ -1,41 +1,17 @@
-#define MAX_USUARIOS 99
-#define MAX_TRANSACAO 99
- 
-typedef struct {
-    char descricao[100]; // Ex.: "Depósito", "Saque", "Transferência"
-    float valor;
-    char dataHora[20];
-    char nome_destinatario[50]; //Quando realiza uma transferencia armazena o nome do destinatario para exibir no extrato
-    char nome_remetente[50]; //Quando realiza uma transferencia armazena o nome do destinatario para exibir no extrato
-} Extrato; //nome da estrutura
+#include "globals.h"
 
-
-//Define uma typedef struct. Uma estrutura de dados que pode conter mais de um tipo de dados, e podemos nomear essa estrutura como quisermos, chamei-a de Dados.
-typedef struct {
-    char nome[100];
-    char cpf[15];
-    char dataNascimento[11];
-    char email[100];
-    char telefone[15];
-    char senha[20];
-    int conta_bancaria;
-    float saldo;
-    int contador_transacoes;
-    Extrato transacao[MAX_TRANSACAO];    
-} Dados; 
-
-// Fiz um cadastro para nós 3 para que pudessemos testar a função de cadastro e de login.
+//Estruturas e variáveis globais
 Dados usuarios[MAX_USUARIOS] = {
     {//indice 0
         .nome = "Francisco Paulino",
         .cpf = "00000000000",
         .dataNascimento = "00/00/0000",
         .email = "francisco@paulino.com",
-        .telefone= "00000000000",
+        .telefone = "00000000000",
         .senha = "000000",
-        .conta_bancaria = 12345678,
+        .conta_bancaria = 0000,
         .saldo = 10000,
-        .transacao = {{}},
+        .transacao = {{0}},
         .contador_transacoes = 0       
     },
     {//indice 1
@@ -43,26 +19,38 @@ Dados usuarios[MAX_USUARIOS] = {
         .cpf = "11111111111",
         .dataNascimento = "00/00/0000",
         .email = "matheus@barbosa.com",
-        .telefone= "00000000000",
-        .senha = "111111",
-        .conta_bancaria = 87654321,
+        .telefone = "00000000000",
+        .senha = "000000",
+        .conta_bancaria = 1111,
         .saldo = 10000,
-        .transacao = {{}},
+        .transacao = {{0}},
         .contador_transacoes = 0
     },
-    {// indice 2
+    {//indice 2
         .nome = "Luan Kennedy",
         .cpf = "22222222222",
         .dataNascimento = "00/00/0000",
         .email = "luan@kennedy.com",
-        .telefone= "00000000000",
-        .senha = "222222",
-        .conta_bancaria = 01234567,
+        .telefone = "00000000000",
+        .senha = "000000",
+        .conta_bancaria = 2222,
         .saldo = 10000,
-        .transacao = {{}},
+        .transacao = {{0}},
+        .contador_transacoes = 0
+    },
+    {//indice 3
+        .nome = "Ramon Rene",
+        .cpf = "33333333333",
+        .dataNascimento = "00/00/0000",
+        .email = "ramon@rene.com",
+        .telefone = "00000000000",
+        .senha = "000000",
+        .conta_bancaria = 3333,
+        .saldo = 10000,
+        .transacao = {{0}},
         .contador_transacoes = 0
     }
 };
 
-int contador_usuarios = 3; //Contador para garantir que novos usuários sejam cadastrados sempre no próximo indice do vetor. O valor é incrementado a cada cadastro de usuário
-char dataHora[20] = ""; // Variável global que irá receber a data e hora quando o usuário efetuar ou receber uma transação.
+int contador_usuarios = 3; // Contador para garantir que novos usuários sejam cadastrados sempre no próximo indice do vetor
+char dataHora[20] = "";    // Variável global para armazenar data e hora de transações
